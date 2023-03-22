f5b9347575a23376bda0c22cfb23ef2b5e931b81
AnhSN <anhs.noel@gmail.com>
2023-03-22 02:08:43 +0100

    add .gitignore

--- database/df_to_sql.py
+++ database/df_to_sql.py
@@ -1,1 +1,6 @@
-import sqlalchemy
+import pandas
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///db.sqlite3')
+df = pandas.read_csv('database/AAPL.csv')
+df.to_sql("AAPL", engine)
